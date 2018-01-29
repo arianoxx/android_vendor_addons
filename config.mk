50491f350e892558dfa23386b2c44bcfe2502d17
Enes Uzun <4lukankalar@gmail.com>
2018-01-29 15:36:41 +0300

    Update Magisk To V15.3 And Magisk Manager To V5.5.5

---
+++ config.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2017 crDroid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Prebuilts
+PRODUCT_PACKAGES += \
+    AdAway \
+    Turbo \
+    Launcher3 \
+    HealthService 
+
+PRODUCT_COPY_FILES += \
+    vendor/addons/prebuilt/system/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/addons/prebuilt/system/fonts/NotoColorEmoji.ttf:system/fonts/NotoColorEmoji.ttf
+
+# Camera Effects for devices without a vendor partition
+ifneq ($(filter shamu,$(TARGET_PRODUCT)),)
+PRODUCT_COPY_FILES +=  \
+    vendor/addons/prebuilt/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/addons/prebuilt/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+endif
+
+DEVICE_PACKAGE_OVERLAYS += vendor/addons/overlay/common
+
+$(call inherit-product, vendor/addons/prebuilt/bootanimation/bootanimation.mk)
+
+# Magisk Manager
+PRODUCT_PACKAGES += \
+    MagiskManager
+
+# Copy Magisk zip
+PRODUCT_COPY_FILES += \
+    vendor/addons/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+endif
+
